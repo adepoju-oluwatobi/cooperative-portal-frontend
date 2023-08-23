200473883b9a0cfbb1791ad5d03082abb9e34962
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-23 05:56:53 +0100

    added workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/honeyland-cooperative-portal-frontend-",
+  base: "/cooperative-portal-frontend",
 });
