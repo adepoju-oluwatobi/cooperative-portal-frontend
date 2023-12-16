c7a3a2e984a2fb56154b8ab64089d89be0e6a7d5
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-12-16 08:58:05 +0100

    modified the contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,23 +8,29 @@ function Contact() {
       <Header />
       <div className="h-[100vh] bg-[url(./assets/loan-bg.jpg)]">
         <div className="p-4 text-white">
-          <div className="flex flex-col justify-center items-center text-center">
-            <form action="" className="flex flex-col bg-black">
+          <div className="flex flex-col justify-center items-center">
+            <form action="" className="flex flex-col w-[280px] m-auto bg-black">
               <p className="font-bold text-2xl text-center">Contact Us</p>
-              <p className="text-xs md:text-base text-center mb-4">
-                We are always eager to welcome you to our society. Send us an
-                Email today.
+              <p className="text-xs md:text-base mb-4">
+                For enquirys and complaints, please contact us via:
               </p>
+              <p>Email:</p>
               {/* <input type="name" placeholder="Name" />
               <input type="email" placeholder="Email" /> */}
               <a
                 id="emailbtn"
                 href="mailto:honeylandwelfaresociety@gmail.com"
                 target="_blank"
-                className="bg-green-500 text-white px-4 py-2 rounded-lg m-auto"
+                className="text-sm text-blue-500 py-2"
               >
-                Get In Touch
+                honeylandwelfaresociety@gmail.com
               </a>
+
+              <div className='mt-4'>
+              <p>Call/Whatsapp campus rep:</p>
+              <p className='mt-2'>Ipaja:</p>
+              <p>Mrs Ajibola: <span>080********</span></p>
+              </div>
             </form>
           </div>
         </div>
